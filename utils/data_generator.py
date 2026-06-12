e870eaaf2b64a6c1731d59b6f586315e0a695eba
Ananda Mazine <anandamazine@MacBook-Air-de-Ananda.local>
2026-06-12 09:19:56 -0300

    Novas atualizações

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -3,8 +3,8 @@ import uuid
 
 def generate_user():
     return {
-        "nome": "Ananda Teste",
-        "email": f"ananda_{uuid.uuid4().hex}@teste.com",
+        "nome": "Usuário Teste",
+        "email": f"usuario_{uuid.uuid4().hex}@teste.com",
         "password": "123456",
         "administrador": "true"
     }
